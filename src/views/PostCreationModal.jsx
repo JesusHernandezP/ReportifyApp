cfc646ac1a5ab0416dff6996d7bb07dd5015c615
Adriana Buitrago <buitragoadr@gmail.com>
2023-05-05 16:51:38 +0200

    Selector categories

--- src/views/PostCreationModal.jsx
+++ src/views/PostCreationModal.jsx
@@ -1,12 +1,10 @@
 import { useNavigate } from 'react-router-dom'
-import { Modal, Input, Button } from 'antd'
+import { Modal, Input, Button, Select } from 'antd'
 
 import useServer from '../hooks/useServer.js'
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import '../index.css'
-
-
 
 function PostCreationModal() {
 
@@ -24,6 +22,12 @@ function PostCreationModal() {
     navigate("/")
   }
 
+  const themes = ['sports', 'politics', 'economy', 'education', 'society', 'techonology', 'culture', 'sciencie', 'gaming', 'medicine']
+  
+  const selectOptions = themes.map((theme) => {
+    return {value:theme, label:theme}
+  })
+
   return (
     <>
       <Modal
@@ -35,33 +39,41 @@ function PostCreationModal() {
         footer={<></>}
       >
         <form onSubmit={handleSubmit} className='form-login'>
+        <label>Título</label>
           <Input
             name="title"
             required
             placeholder="Título"
           />
-          <Input
-            name="theme"
-            required
-            placeholder="Categoría"
+          <label>Categoría</label>
+          <Select
+          name="theme"
+            placeholder="Elige categoría"
+            style={{
+              width: '100%',
+            }}
+            allowClear
+            options= {selectOptions}
           />
-          <Input.TextArea
-            name="content"
-            required
-            placeholder="¿Sobre qué quieres hablar?"
-          />
-          <Form.Group controlId="formFilesm" className="mb-3">
-            <Form.Label></Form.Label>
-            <Form.Control className='upload-file' type="file" name="photo"
-              accept="image/*" size="sm" />
-          </Form.Group>
-          <div className='ant-modal-footer'>
-            <Button type="primary" htmlType="submit">
-              Publicar
-            </Button >
-          </div>
-        </form>
-      </Modal>
+          <label>Contenido</label>
+        <Input.TextArea
+          name="content"
+          required
+          placeholder="¿Sobre qué quieres hablar?"
+        />
+        <label>Imagen</label>
+        <Form.Group controlId="formFilesm" className="mb-3">
+          <Form.Label></Form.Label>
+          <Form.Control className='upload-file' type="file" name="photo"
+            accept="image/*" size="sm" />
+        </Form.Group>
+        <div className='ant-modal-footer'>
+          <Button type="primary" htmlType="submit">
+            Publicar
+          </Button >
+        </div>
+      </form>
+    </Modal >
     </>
   )
 }
