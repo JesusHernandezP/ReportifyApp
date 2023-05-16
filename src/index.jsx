83b40805d6ba5a137b27c3899c00671a915821ff
Adriana Buitrago <buitragoadr@gmail.com>
2023-05-16 19:47:36 +0200

    Cleaning index

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import AuthProvider from './providers/AuthProvider.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { ConfigProvider, theme } from 'antd'
+import { ConfigProvider } from 'antd'
 
 import './index.css'
 
@@ -12,8 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConfigProvider
       theme={{
-        // algorithm: theme.darkAlgorithm,
-        // colorPrimary: '#b57482',
         token: {
           colorPrimary: '#EC6165',
           colorLink: '#EC6165',
