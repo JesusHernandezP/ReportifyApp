6580d6695b08a7e7c9cb7d36ae239a2a4273726a
Adriana Buitrago <buitragoadr@gmail.com>
2023-05-17 13:25:50 +0200

    Reportify logo-text svg

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = ({onToggleTheme}) => {
       <div className='nav-container'>
         <div className='nav-container_division'>
           <img src={'./logo-reportify.svg'} alt='logo' className='logo-reportify' />
-          <p className='text-logo' >Reportify</p>
+          <img src={'./logo-reportify-text.svg'} alt='logo-text' className='logo-reportify' />
         </div>
         <div className='nav-container_division'>
         <ThemeSwitch onChange={onToggleTheme}/>
